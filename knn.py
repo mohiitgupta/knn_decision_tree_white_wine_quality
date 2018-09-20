0ce8461d125e4c20d2a1ff0be64eeb1cb361d7d1
Mohit Gupta <mohit.gupta.cse11@iitbhu.ac.in>
2018-09-20 14:28:16 -0400

    decision tree implementation complete accuracy 56%, f1 score 47%

--- knn.py
+++ knn.py
@@ -1,100 +1,7 @@
-import csv
-import math
-from random import shuffle
 import pickle
-
-def euclidean_distance(point1, point2):
-    sum = 0;
-    for i in range(len(point1)):
-        sum += (point1[i]-point2[i])*(point1[i]-point2[i])
-    sum = math.sqrt(sum)
-    return sum
-
-def cosine_distance(point1, point2):
-    x = 0;
-    y = 0;
-    xy = 0;
-    for i in range(len(point1)):
-        xy += point1[i]*point2[i]
-        x += point1[i]*point1[i]
-        y += point2[i]*point2[i]
-    ans = xy/(math.sqrt(x*y))
-    return ans
-
-def manhattan_distance(point1, point2):
-    distance = 0
-    for i in range(len(point1)):
-        distance += abs(point1[i]-point2[i])
-    return distance
-
-def find_distance(point1, point2):
-    distance = euclidean_distance(point1, point2)
-    # distance = manhattan_distance(point1, point2)
-    # distance = cosine_distance(point1, point2)
-    # print distance
-    return distance
-
-def divide_folds(k, data):
-    if k < 0:
-        return [data]
-
-    fold_size = len(data)/k
-    folds = [data[x:x+fold_size] for x in xrange(0, len(data), fold_size)]
-    if len(folds) == 5:
-        folds[3] += folds[4]
-        del folds[4]
-    return folds
-
-def split_folds_train_test(test_index, folds):
-    test_fold = folds[test_index]
-    train_fold = []
-    for x in range(len(folds)):
-        if x != test_index:
-            train_fold += folds[x]
-
-    return train_fold, test_fold
-
-def split_train_validation(train_data):
-    validation_len = len(train_data)/5
-    validation = train_data[:validation_len]
-    train = train_data[validation_len:]
-    # print validation[validation_len-1]
-    # print train[0]
-    return train, validation
-
-def find_normalization_params(train_data):
-    minValue = [x for x in train_data[0]]
-    maxValue = [x for x in train_data[0]]
-    for example in train_data:
-        for i in range(len(example)):
-            if example[i]>maxValue[i]:
-                # print "max ", example[i], " ", maxValue[i]
-                # break
-                maxValue[i]=example[i]
-            if example[i]<minValue[i]:
-                minValue[i]=example[i]
-
-    return minValue, maxValue
-
-def min_max_norm(value, minValue, maxValue, scale):
-    value = (value-minValue)/(maxValue-minValue)*scale
-    if value < 0:
-        value = 0
-    if value > scale:
-        value = scale
-    return value
-
-def normalize(data, minValue, maxValue, ignored_feature):
-    features, labels = [],[]
-    for example in data:
-        normalized_example = []
-        for j in range(len(minValue)-1):
-            if j not in ignored_feature:
-                normalized_example.append(min_max_norm(example[j], minValue[j], maxValue[j], 1))
-        labels.append(int(example[len(minValue)-1]))
-        features.append(normalized_example)
-
-    return features, labels
+from distances import find_distance
+from preprocessing import *
+from f1_accuracy import calculate_macro_f1_score
 
 def knn_output(test_point, train_points, train_labels, k, is_weighting):
     distance = []
@@ -102,6 +9,12 @@ def knn_output(test_point, train_points, train_labels, k, is_weighting):
         tup = (find_distance(test_point, point), train_labels[i])
         # print tup
         distance.append(tup)
+
+    """
+    Since the number of points is not that huge i.e. in the range of few thousands.
+    Thus sorting the distances and picking the top k distances also has similar performance
+    as using a heap to fetch top k distances. I am using sorting here.
+    """
     distance.sort(key=lambda tup: tup[0])
     # print distance[:k]
     label = [0 for x in range(10)]
@@ -130,23 +43,12 @@ def knn_output(test_point, train_points, train_labels, k, is_weighting):
 
     return predicted_label, predicted_label_2
 
-def shuffle_data():
-    with open('winequality-white.csv', 'rb') as csvfile:
-        csvreader = csv.reader(csvfile, delimiter = ';')
-        # print type(csvreader)
-        data = []
-        for i, row in enumerate(csvreader):
-            if i != 0:
-                row = [float(i) for i in row]
-                data.append(row)
-    shuffle(data)
-    with open('shuffled_data_wine', 'wb') as fp:
-        pickle.dump(data, fp)
-
 def get_results(train, train_labels, test, test_labels, k):
     count = 0
     correct = 0
     incorrect = []
+    predictions = []
+    true_labels = []
     for i, test_point in enumerate(test):
         count += 1
         true_label = test_labels[i]
@@ -157,9 +59,14 @@ def get_results(train, train_labels, test, test_labels, k):
             correct += 1
         else:
             incorrect.append((predicted_label, true_label))
-        # break
 
-    return 100.0*correct/count
+        predictions.append(predicted_label)
+        true_labels.append(true_label)
+
+    accuracy = 100.0*correct/count
+    f1_score = calculate_macro_f1_score(predictions, true_labels)
+
+    return f1_score, accuracy
     # print incorrect
         # break
 
@@ -168,32 +75,91 @@ def main():
     # uncomment the line below to work on original file
     # shuffle_data()
     
-    with open ('shuffled_data_wine', 'rb') as fp:
+    with open ('shuffled_data_wine_knn', 'rb') as fp:
         data = pickle.load(fp)
     # print len(data)
     no_folds = 4
+
+    """"
+    The portion of code which divides into 4 folds for cross validation.
+    The function divide_folds is in preprocessing.py file.
+    """
     folds = divide_folds(no_folds, data)
+
+    k = 12
+    print "Hyper-parameters:"
+    print "K: ", k
+    print "Distance measure: ", "Euclidean Distance"
+    print ""
     
     for i in range(no_folds):
+        print "Fold-", i+1, ":"
+
+        """
+        The function split_folds_train_test takes out the ith fold each time as the test_data which
+        is not touched until after the model is tuned.
+        """
         train_data, test_data = split_folds_train_test(i, folds)
-        # shuffle(train_data)
+
+        """
+        The train_data is split into training and validation using the function spit_train_validation.
+        The implementation of this function is in preprocessing.py
+        """
         train, validation = split_train_validation(train_data)
+
+        """
+        finds the normalization params for the training data
+        """
         minValue, maxValue = find_normalization_params(train_data);
 
         #feature engineering
         # for i in range(11):
-        ignored_feature = [2]
-        train_norm, train_labels = normalize(train, minValue, maxValue, ignored_feature)
-        valid_norm, valid_labels = normalize(validation, minValue, maxValue, ignored_feature)
-        test_norm, test_labels = normalize(test_data, minValue, maxValue, ignored_feature)
 
+        """
+        scale and isInt were tuning parameters which allowed me to change the scale from 0 to 1 to any other scale i.e. 10, 100 etc.
+        isInt is a flag to either trim the values of the features to floor of the double value. This is meaningful only if the scale is reasonably spread
+        out i.e. 100 or 1000 otherwise there will be a lot of loss of information from feature values.
+        """
+        scale = 1
+        isInt = False
+        """
+        I did feature engineering by using ignored_feature list. This list indicates the features which I ignore in my final model.
+        The practice which I did was ignoring each feature one by one and seeing the impact on f1_score. This tells me the contribution of
+        each feature in knowing the correct label. After this experiment, I found that ignoring feature 2 i.e. citric acid content gave me
+        better results than those including it.
+        """
+        ignored_feature = [2]
+        train_norm, train_labels = normalize(train, minValue, maxValue, ignored_feature, scale, isInt)
+        valid_norm, valid_labels = normalize(validation, minValue, maxValue, ignored_feature, scale, isInt)
+        test_norm, test_labels = normalize(test_data, minValue, maxValue, ignored_feature, scale, isInt)
+
+
+        average_validation_f1 = 0
+        average_validation_accuracy = 0
+        average_test_f1 = 0
+        average_test_accuracy = 0
+        """
+        this for loop was used to get scores for a range of k values 
+        and then select the best ones on the validation data set.
+        """
         for k in range(12,13):
             #validation data
             # print "k is ", k, "ignored feature is ", ignored_feature
-            print "accuracy for validation ", get_results(train_norm, train_labels, valid_norm, valid_labels, k)
+            f1_score, accuracy = get_results(train_norm, train_labels, valid_norm, valid_labels, k)
+            print "Validation: F1 Score: ", f1_score, ", Accuracy: ", accuracy
+            average_validation_f1 += f1_score
+            average_validation_accuracy += accuracy
 
             #test data
-            print "accuracy for test ", get_results(train_norm, train_labels, test_norm, test_labels, k)
+            # f1_score, accuracy = get_results(train_norm, train_labels, test_norm, test_labels, k)
+            # print "Test: F1 Score: ", f1_score, ", Accuracy: ", accuracy
+            average_test_f1 += f1_score
+            average_test_accuracy += accuracy
+            print ""
+
+        print "Average:"
+        print "Validation: F1 Score: ", average_validation_f1/4.0, ", Accuracy: ", average_validation_accuracy/4.0
+        # print "Test: F1 Score: ", average_test_f1/4.0, ", Accuracy: ", average_test_accuracy/4.0
 
 if __name__ == '__main__':
     main()
